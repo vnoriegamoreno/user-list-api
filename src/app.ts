c316d1d60dbc1b7fe92ec10e6379a5920400e80e
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2024-10-24 16:57:17 -0600

    task-1 - Enable all CORS

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import userRouter from './routes/userRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use(express.json());
 
-app.get('/hello-world', (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    message: "Hello World",
-  });
-});
+app.use('/api/user', userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
